17e6be1a5a1bd52e252609c264c807a712c6f5c5
Abhishekkumar-wq <abhishekyagik613@gmail.com>
2025-10-09 11:01:07 +0530

    Searching

---
+++ Searching.java
@@ -0,0 +1,20 @@
+public class optimizing {
+    public static void main(String[] args){
+        int numbers[]={1,2,3,4,5,6,7,8,9,10};
+        int target=6;
+        for(int num:numbers){
+            if(num==target){
+                System.out.println("the target is found :"+target);
+                break;
+                
+            }
+            {
+                System.out.println(""+num);
+            }
+
+        }
+
+
+
+    }
+}
